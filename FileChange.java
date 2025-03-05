6ed35a75fe72647f5d89ae96b11291f9b2254d87
Atletl Gomez Vargas <atletlgomez1@gmail.com>
2025-03-05 14:11:43 -0800

    It works creates and names txt files

--- FileChange.java
+++ FileChange.java
@@ -1,12 +1,15 @@
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 
 public class FileChange {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String fileName = args[0]; //user inputs file name
         File outputFile = new File(fileName); // creates file with that name
         FileWriter outputWrite = new FileWriter(outputFile);
         //creates file write to the path 
         outputWrite.write("You named and created this file :3");
+        outputWrite.close();
     }
 }
